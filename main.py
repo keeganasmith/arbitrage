5c3284d08fd7312e99d32bfbd105c20c7dd0f8c2
Keegan Smith <keeganasmith2003@gmail.com>
2024-09-08 01:30:28 -0500

    all games from draftkings and bet365 inserted in table

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ def arb(capital, a_odds, b_odds):
     guarantee_profit = min(a_wins, b_wins)
     return [a_stake, b_stake, guarantee_profit]
 if __name__ == "__main__":
-
-    game_records = Draftkings().get_nfl_games()
-    
+    my_draft_kings = Draftkings()
+    draft_king_records = my_draft_kings.get_nfl_games()
+    my_draft_kings.driver.quit()
+    db.update_nfl_games("nfl_games", draft_king_records)
+    my_bet365 = Bet365()
+    bet365_records = my_bet365.get_nfl_games()
+    db.update_nfl_games("nfl_games", bet365_records)
+    my_bet365.driver.quit()
     #db.update_nfl_games("nfl_games", game_records)
     #result = arb(100, 2.62, 1.68)
     
